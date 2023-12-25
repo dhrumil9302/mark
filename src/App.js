c2aed0264d9a3ed337b679fe6bcefb5e047bc82e
dhrumil9302 <suthardhrumil5@gmail.com>
2023-12-25 18:11:08 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Hero from './Components/Hero.jsx'
+import Navbar from "./Components/Navbar.jsx";
+import Offers from "./Components/Offers.jsx";
+import Plan from "./Components/Plan.jsx";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <Navbar/>
+      <Hero/>
+      <Offers/>
+      <Plan/>
+  </div>
   );
 }
 
